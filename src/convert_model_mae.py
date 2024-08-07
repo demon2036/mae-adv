d66bc521b82e76359ff39f2886b2b9481279df6c
jtitor <1165137798@qq.com>
2024-08-07 14:44:09 +0800

    commit

---
+++ src/convert_model_mae.py
@@ -0,0 +1,249 @@
+import argparse
+import json
+import math
+import os.path
+import re
+
+import jax
+import torch
+from flax.serialization import msgpack_serialize
+
+# jax.distributed.initialize()
+
+from functools import partial
+
+from timm.models import VisionTransformer
+from torch.utils.data import DataLoader
+import einops
+import flax.jax_utils
+
+import jax.numpy as jnp
+import numpy as np
+import orbax
+import orbax.checkpoint as ocp
+
+
+def convert(params, checkpoint):
+    # params = {'model': params}
+    pos_embed = params["model"]["embed"]["wpe"]
+
+    decoder_pos_embed = params["model"]["decoder_pos_embed"]
+
+    pos_embed = pos_embed.reshape(1, -1, pos_embed.shape[-1])
+    wte = params["model"]["embed"]["wte"]["kernel"].transpose(3, 2, 0, 1)
+
+    state_dict = {
+        "patch_embed.proj.weight": wte,
+        "patch_embed.proj.bias": params["model"]["embed"]["wte"]["bias"],
+        "decoder_pred.weight": params["model"]["decoder_pred"]['kernel'].transpose(1, 0),
+        "decoder_pred.bias": params["model"]["decoder_pred"]["bias"],
+
+        "decoder_embed.weight": params["model"]["decoder_embed"]['kernel'].transpose(1, 0),
+        "decoder_embed.bias": params["model"]["decoder_embed"]["bias"],
+        'mask_token': params["model"]['mask_token']
+
+    }
+
+    if "cls_token" in params["model"]["embed"]:
+        print("Use cls token")
+        state_dict["cls_token"] = params["model"]["embed"]["cls_token"]
+        # state_dict["pos_embed"] = jnp.pad(pos_embed, ((0, 0), (1, 0), (0, 0)))
+        # print(state_dict["pos_embed"].shape)
+    else:
+        print("Use Mean Pooling")
+
+    state_dict["pos_embed"] = pos_embed
+    state_dict["decoder_pos_embed"] = decoder_pos_embed
+
+    print(pos_embed.shape, state_dict["pos_embed"].shape)
+
+    if "norm" in params["model"]:
+        print('use norm')
+        state_dict["norm.weight"] = params["model"]["norm"]["scale"]
+        state_dict["norm.bias"] = params["model"]["norm"]["bias"]
+
+    if "decoder_norm" in params["model"]:
+        print('use norm')
+        state_dict["decoder_norm.weight"] = params["model"]["decoder_norm"]["scale"]
+        state_dict["decoder_norm.bias"] = params["model"]["decoder_norm"]["bias"]
+
+    if "fc_norm" in params["model"]:
+        print('use fc_norm')
+        state_dict["fc_norm.weight"] = params["model"]["fc_norm"]["scale"]
+        state_dict["fc_norm.bias"] = params["model"]["fc_norm"]["bias"]
+    # if "head" in params["model"] and not args.exclude_heads:
+    #     state_dict["head.weight"] = params["model"]["head"]["kernel"].transpose(1, 0)
+    #     state_dict["head.bias"] = params["model"]["head"]["bias"]
+
+    if "head" in params["model"]:
+        state_dict["head.weight"] = params["model"]["head"]["kernel"].transpose(1, 0)
+        state_dict["head.bias"] = params["model"]["head"]["bias"]
+        print(params["model"]["head"]["kernel"].shape)
+
+    for name, layer in params["model"].items():
+        if not name.startswith("layer_"):
+            continue
+        layer_idx = int(name[6:])
+
+        if "q_norm" in layer["attn"]:
+            print('use qk_norm')
+            state_dict[f"blocks.{layer_idx}.attn.q_norm.weight"] = layer["attn"]["q_norm"]["scale"]
+            state_dict[f"blocks.{layer_idx}.attn.q_norm.bias"] = layer["attn"]["q_norm"]["bias"]
+            state_dict[f"blocks.{layer_idx}.attn.k_norm.weight"] = layer["attn"]["k_norm"]["scale"]
+            state_dict[f"blocks.{layer_idx}.attn.k_norm.bias"] = layer["attn"]["k_norm"]["bias"]
+        else:
+            print('Do not use qk_norm')
+
+        wq = layer["attn"]["wq"]["kernel"]
+        wk = layer["attn"]["wk"]["kernel"]
+        wv = layer["attn"]["wv"]["kernel"]
+        wo = layer["attn"]["wo"]["kernel"]
+
+        wq = wq.reshape(wq.shape[0], -1)
+        wk = wk.reshape(wk.shape[0], -1)
+        wv = wv.reshape(wv.shape[0], -1)
+        wo = wo.reshape(wv.shape[0], -1)
+        qkv = jnp.concatenate((wq, wk, wv), axis=1).transpose(1, 0)
+
+        state_dict[f"blocks.{layer_idx}.attn.qkv.weight"] = qkv
+        state_dict[f"blocks.{layer_idx}.attn.qkv.bias"] = jnp.concatenate(
+            (
+                layer["attn"]["wq"]["bias"].reshape(-1),
+                layer["attn"]["wk"]["bias"].reshape(-1),
+                layer["attn"]["wv"]["bias"].reshape(-1),
+            ),
+        )
+        state_dict[f"blocks.{layer_idx}.attn.proj.weight"] = wo.transpose(1, 0)
+        state_dict[f"blocks.{layer_idx}.attn.proj.bias"] = layer["attn"]["wo"]["bias"]
+
+        fc1 = layer["ff"]["w1"]["kernel"].transpose(1, 0)
+        fc2 = layer["ff"]["w2"]["kernel"].transpose(1, 0)
+        state_dict[f"blocks.{layer_idx}.mlp.fc1.weight"] = fc1
+        state_dict[f"blocks.{layer_idx}.mlp.fc1.bias"] = layer["ff"]["w1"]["bias"]
+        state_dict[f"blocks.{layer_idx}.mlp.fc2.weight"] = fc2
+        state_dict[f"blocks.{layer_idx}.mlp.fc2.bias"] = layer["ff"]["w2"]["bias"]
+
+        state_dict[f"blocks.{layer_idx}.norm1.weight"] = layer["norm1"]["scale"]
+        state_dict[f"blocks.{layer_idx}.norm1.bias"] = layer["norm1"]["bias"]
+        state_dict[f"blocks.{layer_idx}.norm2.weight"] = layer["norm2"]["scale"]
+        state_dict[f"blocks.{layer_idx}.norm2.bias"] = layer["norm2"]["bias"]
+
+        if "scale1" in layer:
+            state_dict[f"blocks.{layer_idx}.ls1.gamma"] = layer["scale1"]
+        if "scale2" in layer:
+            state_dict[f"blocks.{layer_idx}.ls2.gamma"] = layer["scale2"]
+
+    for name, layer in params["model"].items():
+        if not name.startswith("decoder_layer_"):
+            continue
+        print(len('decoder_layer_'))
+        layer_idx = int(name[14:])
+
+        if "q_norm" in layer["attn"]:
+            print('use qk_norm')
+            state_dict[f"decoder_blocks.{layer_idx}.attn.q_norm.weight"] = layer["attn"]["q_norm"]["scale"]
+            state_dict[f"decoder_blocks.{layer_idx}.attn.q_norm.bias"] = layer["attn"]["q_norm"]["bias"]
+            state_dict[f"decoder_blocks.{layer_idx}.attn.k_norm.weight"] = layer["attn"]["k_norm"]["scale"]
+            state_dict[f"decoder_blocks.{layer_idx}.attn.k_norm.bias"] = layer["attn"]["k_norm"]["bias"]
+        else:
+            print('Do not use qk_norm')
+
+        wq = layer["attn"]["wq"]["kernel"]
+        wk = layer["attn"]["wk"]["kernel"]
+        wv = layer["attn"]["wv"]["kernel"]
+        wo = layer["attn"]["wo"]["kernel"]
+
+        wq = wq.reshape(wq.shape[0], -1)
+        wk = wk.reshape(wk.shape[0], -1)
+        wv = wv.reshape(wv.shape[0], -1)
+        wo = wo.reshape(wv.shape[0], -1)
+        qkv = jnp.concatenate((wq, wk, wv), axis=1).transpose(1, 0)
+
+        state_dict[f"decoder_blocks.{layer_idx}.attn.qkv.weight"] = qkv
+        state_dict[f"decoder_blocks.{layer_idx}.attn.qkv.bias"] = jnp.concatenate(
+            (
+                layer["attn"]["wq"]["bias"].reshape(-1),
+                layer["attn"]["wk"]["bias"].reshape(-1),
+                layer["attn"]["wv"]["bias"].reshape(-1),
+            ),
+        )
+        state_dict[f"decoder_blocks.{layer_idx}.attn.proj.weight"] = wo.transpose(1, 0)
+        state_dict[f"decoder_blocks.{layer_idx}.attn.proj.bias"] = layer["attn"]["wo"]["bias"]
+
+        fc1 = layer["ff"]["w1"]["kernel"].transpose(1, 0)
+        fc2 = layer["ff"]["w2"]["kernel"].transpose(1, 0)
+        state_dict[f"decoder_blocks.{layer_idx}.mlp.fc1.weight"] = fc1
+        state_dict[f"decoder_blocks.{layer_idx}.mlp.fc1.bias"] = layer["ff"]["w1"]["bias"]
+        state_dict[f"decoder_blocks.{layer_idx}.mlp.fc2.weight"] = fc2
+        state_dict[f"decoder_blocks.{layer_idx}.mlp.fc2.bias"] = layer["ff"]["w2"]["bias"]
+
+        state_dict[f"decoder_blocks.{layer_idx}.norm1.weight"] = layer["norm1"]["scale"]
+        state_dict[f"decoder_blocks.{layer_idx}.norm1.bias"] = layer["norm1"]["bias"]
+        state_dict[f"decoder_blocks.{layer_idx}.norm2.weight"] = layer["norm2"]["scale"]
+        state_dict[f"decoder_blocks.{layer_idx}.norm2.bias"] = layer["norm2"]["bias"]
+
+        if "scale1" in layer:
+            state_dict[f"decoder_blocks.{layer_idx}.ls1.gamma"] = layer["scale1"]
+        if "scale2" in layer:
+            state_dict[f"decoder_blocks.{layer_idx}.ls2.gamma"] = layer["scale2"]
+
+    print(params['model'].keys())
+    # print(state_dict.keys())
+    # while True:
+    #     pass
+
+    state_dict = {k: torch.tensor(np.asarray(v)) for k, v in state_dict.items()}
+
+    print(state_dict.keys())
+
+    torch.save(state_dict, checkpoint)  #args.checkpoint.replace(".msgpack", ".pth")
+    return state_dict
+
+
+def train_and_evaluate(args):
+    """Execute model training and evaluation loop.
+
+  Args:
+    config: Hyperparameter configuration for training and evaluation.
+    workdir: Directory where the tensorboard summaries are written to.
+
+  Returns:
+    The train state (which includes the `.params`).
+  """
+
+    checkpointer = ocp.AsyncCheckpointer(ocp.PyTreeCheckpointHandler())
+    # checkpointer = ocp.PyTreeCheckpointer()
+    print(os.getcwd() + '/' + args.pretrained_model)
+    if args.restore_type == 'orbax':
+        state = checkpointer.restore(os.getcwd() + '/' + args.pretrained_model, )['model']
+        params = state['params']
+    else:
+        with open(args.pretrained_model, "rb") as fp:
+            params = flax.serialization.msgpack_restore(fp.read())
+
+    state_dict = convert(params, args.checkpoint)
+
+    return state_dict
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pretrained-model", type=str,
+                        default='checkpoints/mae-deit-s2-32-in1k-1600ep-d8-ema')
+    parser.add_argument("--checkpoint", type=str,
+                        default='checkpoints/mae-deit-s2-32-in1k-1600ep-d8-ema'
+                                '.pth')
+
+    # parser.add_argument("--pretrained-model", type=str,
+    #                     default='checkpoint/jax_model/best/cifar100/vit-l2-32-cifar100-6000ep-ls0.4-gap-beta5-ema')
+    # parser.add_argument("--checkpoint", type=str,
+    #                     default='checkpoint/pytorch_model/best/cifar100/vit-l2-32-cifar100-6000ep-ls0.4-gap-beta5-ema'
+    #                             '.pth')
+
+    parser.add_argument("--restore-type", type=str,
+                        default='orbax',
+                        # default='no_orbax',
+                        )
+
+    args = parser.parse_args()
+    train_and_evaluate(args)
---
+++ src/test.py
@@ -0,0 +1,283 @@
+import argparse
+import json
+import math
+import os.path
+import random
+import re
+
+import jax
+import torch
+from flax.serialization import msgpack_serialize
+
+# jax.distributed.initialize()
+
+from functools import partial
+
+from timm.models import VisionTransformer
+from torch.utils.data import DataLoader
+import einops
+import flax.jax_utils
+
+import jax.numpy as jnp
+import numpy as np
+import orbax
+import orbax.checkpoint as ocp
+from torchvision.transforms import ToTensor, PILToTensor
+
+from src.training import create_mae_train_state
+from torchvision.datasets import CIFAR10
+
+
+def convert(params, checkpoint):
+    print(params)
+
+    while True:
+        pass
+
+    params = {'model': params}
+    pos_embed = params["model"]["embed"]["wpe"]
+    pos_embed = pos_embed.reshape(1, -1, pos_embed.shape[-1])
+    wte = params["model"]["embed"]["wte"]["kernel"].transpose(3, 2, 0, 1)
+
+    state_dict = {
+        "patch_embed.proj.weight": wte,
+        "patch_embed.proj.bias": params["model"]["embed"]["wte"]["bias"],
+    }
+
+    if "cls_token" in params["model"]["embed"]:
+        print("Use cls token")
+        state_dict["cls_token"] = params["model"]["embed"]["cls_token"]
+        state_dict["pos_embed"] = jnp.pad(pos_embed, ((0, 0), (1, 0), (0, 0)))
+    else:
+        state_dict["pos_embed"] = pos_embed
+        print("Use Mean Pooling")
+
+    print(pos_embed.shape, state_dict["pos_embed"].shape)
+
+    if "norm" in params["model"]:
+        print('use norm')
+        state_dict["norm.weight"] = params["model"]["norm"]["scale"]
+        state_dict["norm.bias"] = params["model"]["norm"]["bias"]
+
+    if "fc_norm" in params["model"]:
+        print('use fc_norm')
+        state_dict["fc_norm.weight"] = params["model"]["fc_norm"]["scale"]
+        state_dict["fc_norm.bias"] = params["model"]["fc_norm"]["bias"]
+    # if "head" in params["model"] and not args.exclude_heads:
+    #     state_dict["head.weight"] = params["model"]["head"]["kernel"].transpose(1, 0)
+    #     state_dict["head.bias"] = params["model"]["head"]["bias"]
+
+    if "head" in params["model"]:
+        state_dict["head.weight"] = params["model"]["head"]["kernel"].transpose(1, 0)
+        state_dict["head.bias"] = params["model"]["head"]["bias"]
+        print(params["model"]["head"]["kernel"].shape)
+
+    for name, layer in params["model"].items():
+        if not name.startswith("layer_"):
+            continue
+        layer_idx = int(name[6:])
+
+        if "q_norm" in layer["attn"]:
+            print('use qk_norm')
+            state_dict[f"blocks.{layer_idx}.attn.q_norm.weight"] = layer["attn"]["q_norm"]["scale"]
+            state_dict[f"blocks.{layer_idx}.attn.q_norm.bias"] = layer["attn"]["q_norm"]["bias"]
+            state_dict[f"blocks.{layer_idx}.attn.k_norm.weight"] = layer["attn"]["k_norm"]["scale"]
+            state_dict[f"blocks.{layer_idx}.attn.k_norm.bias"] = layer["attn"]["k_norm"]["bias"]
+        else:
+            print('Do not use qk_norm')
+
+        wq = layer["attn"]["wq"]["kernel"]
+        wk = layer["attn"]["wk"]["kernel"]
+        wv = layer["attn"]["wv"]["kernel"]
+        wo = layer["attn"]["wo"]["kernel"]
+
+        wq = wq.reshape(wq.shape[0], -1)
+        wk = wk.reshape(wk.shape[0], -1)
+        wv = wv.reshape(wv.shape[0], -1)
+        wo = wo.reshape(wv.shape[0], -1)
+        qkv = jnp.concatenate((wq, wk, wv), axis=1).transpose(1, 0)
+
+        state_dict[f"blocks.{layer_idx}.attn.qkv.weight"] = qkv
+        state_dict[f"blocks.{layer_idx}.attn.qkv.bias"] = jnp.concatenate(
+            (
+                layer["attn"]["wq"]["bias"].reshape(-1),
+                layer["attn"]["wk"]["bias"].reshape(-1),
+                layer["attn"]["wv"]["bias"].reshape(-1),
+            ),
+        )
+        state_dict[f"blocks.{layer_idx}.attn.proj.weight"] = wo.transpose(1, 0)
+        state_dict[f"blocks.{layer_idx}.attn.proj.bias"] = layer["attn"]["wo"]["bias"]
+
+        fc1 = layer["ff"]["w1"]["kernel"].transpose(1, 0)
+        fc2 = layer["ff"]["w2"]["kernel"].transpose(1, 0)
+        state_dict[f"blocks.{layer_idx}.mlp.fc1.weight"] = fc1
+        state_dict[f"blocks.{layer_idx}.mlp.fc1.bias"] = layer["ff"]["w1"]["bias"]
+        state_dict[f"blocks.{layer_idx}.mlp.fc2.weight"] = fc2
+        state_dict[f"blocks.{layer_idx}.mlp.fc2.bias"] = layer["ff"]["w2"]["bias"]
+
+        state_dict[f"blocks.{layer_idx}.norm1.weight"] = layer["norm1"]["scale"]
+        state_dict[f"blocks.{layer_idx}.norm1.bias"] = layer["norm1"]["bias"]
+        state_dict[f"blocks.{layer_idx}.norm2.weight"] = layer["norm2"]["scale"]
+        state_dict[f"blocks.{layer_idx}.norm2.bias"] = layer["norm2"]["bias"]
+
+        if "scale1" in layer:
+            state_dict[f"blocks.{layer_idx}.ls1.gamma"] = layer["scale1"]
+        if "scale2" in layer:
+            state_dict[f"blocks.{layer_idx}.ls2.gamma"] = layer["scale2"]
+
+    state_dict = {k: torch.tensor(np.asarray(v)) for k, v in state_dict.items()}
+
+    print(state_dict.keys())
+
+    torch.save(state_dict, checkpoint)  # args.checkpoint.replace(".msgpack", ".pth")
+    return state_dict
+
+
+def train_and_evaluate(args):
+    """Execute model training and evaluation loop.
+
+  Args:
+    config: Hyperparameter configuration for training and evaluation.
+    workdir: Directory where the tensorboard summaries are written to.
+
+  Returns:
+    The train state (which includes the `.params`).
+  """
+
+    state = create_mae_train_state(args)
+
+    checkpointer = ocp.AsyncCheckpointer(ocp.PyTreeCheckpointHandler())
+    # checkpointer = ocp.PyTreeCheckpointer()
+    print(os.getcwd() + '/' + args.pretrained_model)
+    if args.restore_type == 'orbax':
+        ckpt = {'model': state}
+
+        # state = checkpointer.restore(os.getcwd() + '/' + args.pretrained_model)['model']
+
+        state = checkpointer.restore(os.getcwd() + '/' + args.pretrained_model, item=ckpt)['model']
+
+        # state = checkpointer.restore(os.getcwd() + '/' + args.pretrained_model, item=ckpt)['model']
+        # params = state['params']
+        # print(params['model'].keys())
+
+    dataset = CIFAR10('../data', train=True, transform=PILToTensor())
+    dataloader = DataLoader(dataset, batch_size=2)
+
+    import matplotlib.pyplot as plt
+    import einops
+
+    for data in dataloader:
+        x, y = data
+
+        data_jax = jnp.array(np.array(x))
+
+        data = einops.rearrange(x, 'b c h w ->b h w c')
+        rngs, updates = state.split_rngs()
+        rngs['random_masking']+=1
+        print(rngs)
+        metric = state.apply_fn({'params': state.params}, data_jax, rngs=rngs)
+        #{'mse_loss': jnp.mean(loss), 'pred': pred, 'mask': mask}
+        pred = metric['pred']
+        mask = metric['mask']
+
+        print(metric['mse_loss'])
+
+        data_jax_flat = einops.rearrange(data_jax, 'b c (h d1) (w d2) -> b (h w) (d1 d2  c)', d1=2, d2=2)
+        mask = jnp.astype(mask, jnp.int32)
+        print(pred.shape, mask.shape)
+
+        print(data_jax.max(), pred.max())
+
+        out = mask[:, :, None] * data_jax_flat / 255 + (1 - mask[:, :, None]) * pred
+        out = mask[:, :, None] * pred + (1 - mask[:, :, None]) * data_jax_flat / 255
+
+        # while True:
+        #     pass
+
+        pred = einops.rearrange(pred, 'b (h w) (d1 d2 c )-> b (h d1) (w d2) c ', d1=2, d2=2, h=16)
+        out = einops.rearrange(out, 'b (h w) (d1 d2 c )-> b (h d1) (w d2) c ', d1=2, d2=2, h=16)
+
+        # print(pred)
+        plt.subplot(131)
+        plt.imshow(pred[0])
+
+        plt.subplot(132)
+        plt.imshow(data[0])
+
+        plt.subplot(133)
+        plt.imshow(out[0])
+
+        plt.show()
+
+    return state
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pretrained-model", type=str,
+                        default='./checkpoints/mae-deit-s2-32-in1k-1600ep-d1-ema')
+
+    parser.add_argument("--restore-type", type=str,
+                        default='orbax',
+                        # default='no_orbax',
+                        )
+
+    parser.add_argument("--train-dataset-shards")
+    parser.add_argument("--valid-dataset-shards")
+    parser.add_argument("--train-batch-size", type=int, default=2048)
+    parser.add_argument("--valid-batch-size", type=int, default=256)
+    parser.add_argument("--train-loader-workers", type=int, default=40)
+    parser.add_argument("--valid-loader-workers", type=int, default=5)
+
+    parser.add_argument("--random-crop", default="rrc")
+    parser.add_argument("--color-jitter", type=float, default=0.0)
+    parser.add_argument("--auto-augment", default="none")
+    parser.add_argument("--random-erasing", type=float, default=0.25)
+    parser.add_argument("--augment-repeats", type=int, default=3)
+    parser.add_argument("--test-crop-ratio", type=float, default=0.875)
+
+    parser.add_argument("--mixup", type=float, default=0.8)
+    parser.add_argument("--cutmix", type=float, default=1.0)
+    parser.add_argument("--criterion", default="ce")
+    parser.add_argument("--label-smoothing", type=float, default=0.1)
+
+    parser.add_argument("--layers", type=int, default=12)
+    parser.add_argument("--decoder_layers", type=int, default=1)
+    parser.add_argument("--dim", type=int, default=384)
+    parser.add_argument("--heads", type=int, default=3)
+    parser.add_argument("--labels", type=int, default=-1)
+    parser.add_argument("--layerscale", action="store_true", default=False)
+    parser.add_argument("--patch-size", type=int, default=2)
+    parser.add_argument("--image-size", type=int, default=32)
+    parser.add_argument("--posemb", default="sincos2d")
+    parser.add_argument("--pooling", default="cls")
+    parser.add_argument("--dropout", type=float, default=0.0)
+    parser.add_argument("--droppath", type=float, default=0.0)
+    parser.add_argument("--grad-ckpt", action="store_true", default=False)
+
+    parser.add_argument("--init-seed", type=int, default=random.randint(0, 1000000))
+    parser.add_argument("--mixup-seed", type=int, default=random.randint(0, 1000000))
+    parser.add_argument("--dropout-seed", type=int, default=random.randint(0, 1000000))
+    parser.add_argument("--shuffle-seed", type=int, default=random.randint(0, 1000000))
+    parser.add_argument("--pretrained-ckpt")
+    parser.add_argument("--label-mapping")
+
+    parser.add_argument("--optimizer", default="adamw")
+    parser.add_argument("--learning-rate", type=float, default=1e-3)
+    parser.add_argument("--weight-decay", type=float, default=0.05)
+    parser.add_argument("--adam-b1", type=float, default=0.9)
+    parser.add_argument("--adam-b2", type=float, default=0.999)
+    parser.add_argument("--adam-eps", type=float, default=1e-8)
+    parser.add_argument("--lr-decay", type=float, default=1.0)
+    parser.add_argument("--clip-grad", type=float, default=0.0)
+    parser.add_argument("--grad-accum", type=int, default=1)
+    parser.add_argument("--use-kan", action="store_true", default=False)
+    parser.add_argument("--polynomial-degree", type=int, default=8)
+
+    parser.add_argument("--warmup-steps", type=int, default=10000)
+    parser.add_argument("--training-steps", type=int, default=200000)
+    parser.add_argument("--log-interval", type=int, default=50)
+    parser.add_argument("--eval-interval", type=int, default=0)
+
+    args = parser.parse_args()
+    train_and_evaluate(args)
